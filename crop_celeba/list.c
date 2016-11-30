9f41200f0ea04369b14b0d351493da941034b12a
张垚 <yaozhang.chn@gmail.com>
2016-11-30 08:57:06 +0800

    Update list.c

--- crop_celeba/list.c
+++ crop_celeba/list.c
@@ -2,48 +2,6 @@
 #include <string.h>
 #include "list.h"
 
-/*
-void list_insert(list *l, void *val)
-{
-    node *new = (node *)malloc(sizeof(node));
-    new->val = (void *)val;
-    new->next = 0;
-
-    if(!l->back){
-        l->front = new;
-        new->prev = 0;
-    }else{
-        l->back->next = new;
-        new->prev = l->back;
-    }
-    l->back = new;
-    ++l->size;
-}
-*/
-/*
-list *make_list()
-{
-    list *l = (list *)malloc(sizeof(list));
-	l->size = 0;
-	l->front = 0;
-	l->back = 0;
-	return l;
-}
-*/
-/*
-void transfer_node(list *s, list *d, node *n)
-{
-    node *prev, *next;
-    prev = n->prev;
-    next = n->next;
-    if(prev) prev->next = next;
-    if(next) next->prev = prev;
-    --s->size;
-    if(s->front == n) s->front = next;
-    if(s->back == n) s->back = prev;
-}
-*/
-
 void *list_pop(list *l){
     if(!l->back) return 0;
     node *b = l->back;
